cfca1d9438b0e3d7a9ceca481c3abc4bb92b855a
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 08:00:34 -0400

    Margherita.py
    Hit Enter to Hear the Song: 'Margherita' by Riccardo Cocciante

---
+++ import-webbrowser-new-2-tabUrl-http/grooveshark.com/s/Margherita/Margherita.py
@@ -0,0 +1,5 @@
+import webbrowser
+new=2
+tabUrl="http://grooveshark.com/#!/s/Margherita/3eAE6n?src=5";
+term= raw_input("Hit Enter to Hear the Song: 'Margherita' by Riccardo Cocciante");
+webbrowser.open(tabUrl+term,new=new);
