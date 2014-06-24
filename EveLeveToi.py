c87ab5da48b6dd83a53184e2ae30ce2680e89c1b
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 08:36:24 -0400

    EveLeveToi.py
    Hit Enter the Song: 'Eve Leve Toi' by Julie Pietri

---
+++ EveLeveToi.py
@@ -0,0 +1,5 @@
+import webbrowser
+new=2
+tabUrl="http://grooveshark.com/s/Eve+L+ve+Toi/mZKOo?src=5";
+term= raw_input("Hit Enter the Song: 'Eve Leve Toi' by Julie Pietri");
+webbrowser.open(tabUrl+term,new=new);
