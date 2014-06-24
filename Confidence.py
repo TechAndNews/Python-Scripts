533a595e9a9479f0be6f269f27d8f94aa7535cf5
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 08:22:50 -0400

    Confidence.py
    Hit Enter the Song: 'Confidence Pour Confidence' by Jean Schultheis

---
+++ Confidence.py
@@ -0,0 +1,5 @@
+import webbrowser
+new=2
+tabUrl="http://grooveshark.com/#!/s/Confidence+Pour+Confidence/4BsDVe?src=5";
+term= raw_input("Hit Enter the Song: 'Confidence Pour Confidence' by Jean Schultheis");
+webbrowser.open(tabUrl+term,new=new);
