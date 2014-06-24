141dd9c9ac95c9b2925972c4d5df021d5274ff80
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 08:13:21 -0400

    Sage.py
    Hit Enter the Song: 'Sage Comme Une image' by Lio

---
+++ Sage.py
@@ -0,0 +1,5 @@
+import webbrowser
+new=2
+tabUrl="http://grooveshark.com/s/Sage+Comme+Une+Image/3yUSjW?src=5";
+term= raw_input("Hit Enter the Song: 'Sage Comme Une image' by Lio");
+webbrowser.open(tabUrl+term,new=new);
