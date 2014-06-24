b931488e86dbd78282733bcd39a98daab1dc9a60
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 09:14:48 -0400

    Update Sage.py

--- Sage.py
+++ Sage.py
@@ -1,5 +1,5 @@
 import webbrowser
 new=2
 tabUrl="http://grooveshark.com/s/Sage+Comme+Une+Image/3yUSjW?src=5";
-term= raw_input("Hit Enter the Song: 'Sage Comme Une image' by Lio");
+term= raw_input("Hit Enter to Hear the Song: 'Sage Comme Une image' by Lio");
 webbrowser.open(tabUrl+term,new=new);
