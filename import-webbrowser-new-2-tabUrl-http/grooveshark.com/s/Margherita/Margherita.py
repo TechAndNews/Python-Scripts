06c572e17d0c475688d8806a4a227a9a04ad7c66
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 08:02:39 -0400

    Delete Margherita.py

--- import-webbrowser-new-2-tabUrl-http/grooveshark.com/s/Margherita/Margherita.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-new=2
-tabUrl="http://grooveshark.com/#!/s/Margherita/3eAE6n?src=5";
-term= raw_input("Hit Enter to Hear the Song: 'Margherita' by Riccardo Cocciante");
-webbrowser.open(tabUrl+term,new=new);
