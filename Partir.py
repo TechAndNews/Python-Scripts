dbe08a7df3f7f5cde4138517c8455abb9a104f3e
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 09:14:16 -0400

    Update Partir.py

--- Partir.py
+++ Partir.py
@@ -1,5 +1,5 @@
 import webbrowser
 new=2
 tabUrl="http://grooveshark.com/s/Partir/4Gumyy?src=5";
-term= raw_input("Hit Enter the Song: 'Partir' by Julien Clerc");
+term= raw_input("Hit Enter to Hear the Song: 'Partir' by Julien Clerc");
 webbrowser.open(tabUrl+term,new=new);
