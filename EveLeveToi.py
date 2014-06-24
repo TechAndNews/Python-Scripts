896f6ad501156de8bf9a89df308de13ffaeeb728
TechAndNews <TechAndNews@Ymail.com>
2014-06-24 09:13:52 -0400

    Update EveLeveToi.py

--- EveLeveToi.py
+++ EveLeveToi.py
@@ -1,5 +1,5 @@
 import webbrowser
 new=2
 tabUrl="http://grooveshark.com/s/Eve+L+ve+Toi/mZKOo?src=5";
-term= raw_input("Hit Enter the Song: 'Eve Leve Toi' by Julie Pietri");
+term= raw_input("Hit Enter to Hear the Song: 'Eve Leve Toi' by Julie Pietri");
 webbrowser.open(tabUrl+term,new=new);
